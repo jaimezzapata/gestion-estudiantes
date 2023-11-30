c42d4ef7d167f6120c4bd2fc560d72815ad2fb38
jaimezzapata <zapataval2304@gmail.com>
2023-11-30 15:26:31 -0500

    Se crea funcionalidad de login con alertas de sweetalert

--- src/components/layouts/auth/Login.jsx
+++ src/components/layouts/auth/Login.jsx
@@ -1,19 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./Login.css";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const Login = () => {
   const [usuarios, setUsuarios] = useState([]);
-
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
   async function getUsuarios() {
     let resultado = await axios.get("http://localhost:3100/usuarios");
     setUsuarios(resultado.data);
     console.log(resultado.data);
   }
-
   useEffect(() => {
     getUsuarios();
   }, []);
+
+  const buscarUsuario = () => {
+    let estado = usuarios.some((usuario) => {
+      if (usuario.user == user && usuario.password == password) {
+        return true;
+      }
+    });
+    return estado;
+  };
+  function iniciarSesion() {
+    if (buscarUsuario()) {
+      Swal.fire({
+        icon: "success",
+        title: "Yeah...",
+        text: "Inicio de sesión correcta...",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Error de credenciales",
+      });
+    }
+  }
 
   return (
     <div className="login-container">
@@ -21,9 +46,21 @@ const Login = () => {
         <h2>Iniciar Sesión</h2>
       </div>
       <form className="login-form">
-        <input type="text" placeholder="Usuario" required />
-        <input type="password" placeholder="Contraseña" required />
-        <button type="submit">Iniciar Sesión</button>
+        <input
+          onChange={(e) => setUser(e.target.value)}
+          type="text"
+          placeholder="Usuario"
+          required
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Contraseña"
+          required
+        />
+        <button onClick={iniciarSesion} type="button">
+          Iniciar Sesión
+        </button>
       </form>
     </div>
   );
