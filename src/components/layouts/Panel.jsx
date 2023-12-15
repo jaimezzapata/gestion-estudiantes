1022a8dd8b0457943f902b1a9237ca1d4d58801d
jaimezzapata <zapataval2304@gmail.com>
2023-12-15 10:26:20 -0500

    Se crea panel de control

--- src/components/layouts/Panel.jsx
+++ src/components/layouts/Panel.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./Panel.css";
 
 const Panel = () => {
   return (
-    <div>Panel</div>
-  )
-}
+    <div className="panel-container">
+      <div className="panel">
+        <h2>Panel de Control</h2>
+        <button className="control-button">Gestion Estudiantes</button>
+        <button className="control-button">Gestión Materias</button>
+        <button className="control-button">
+          <Link to={'/'}>Cerrar Sesion</Link>
+        </button>
+      </div>
+      <section className="contenido">
+        <h1>Métricas de gestión Escolar</h1>
+      </section>
+    </div>
+  );
+};
 
-export default Panel
+export default Panel;
